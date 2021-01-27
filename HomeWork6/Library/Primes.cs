e3e56f4ed072ab7b1ae9729d949a5eb38ddf7980
MariiaRev <maria.revenko2209@gmail.com>
2021-01-27 08:18:04 +0200

    Added library for the 6th homework with classes for accepting user input and searching primes

---
+++ HomeWork6/Library/Primes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Library
+{
+    public static class Primes
+    {
+        public static List<int> SearchByLinq(List<int> numbers)
+        {
+            return numbers.Where(n => n >= 2 && Enumerable.Range(2, n - 2).All(divisor => n % divisor != 0)).ToList();
+        }
+
+        public static List<int> SearchByPLinq(List<int> numbers)
+        {
+            return numbers.AsParallel()
+                          .AsOrdered()
+                          .Where(n => n >= 2 && Enumerable.Range(2, n - 2).All(divisor => n % divisor != 0))
+                          .ToList();
+        }
+
+        public static List<int> GenerateListForPrimes(int start, int end)
+        {
+            if (end < start || (start < 2 && end < 2))
+                return new List<int>();
+
+            if (start < 2)
+            {
+                return Enumerable.Range(2, end - 1).ToList();
+            }
+            
+            return Enumerable.Range(start, end - start + 1).ToList();
+        }
+    }
+}
---
+++ HomeWork6/Library/UserInput.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public static class UserInput
+    {
+        /// <summary>
+        /// Accept user input from console window, as long as the input is not parsed to int.
+        /// </summary>
+        /// <param name="message">Message shown when prompted for user input</param>
+        /// <returns>User input of type int</returns>
+        public static int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            var data = Console.ReadLine().Trim();
+            int number;
+
+            while (!int.TryParse(data, out number))
+            {
+                Console.WriteLine($"\n\nYou need to enter an integer number. Try again:");
+                data = Console.ReadLine().Trim();
+            }
+
+            return number;
+        }
+
+        /// <summary>
+        /// Accept user input from console window in a range from start to end (inclusive), as long as the input is not parsed to int.
+        /// </summary>
+        /// <param name="message">Message shown when prompted for user input</param>
+        /// <param name="start">Lower input limit (inclusive)</param>
+        /// <param name="end">Upper input limit (inclusive)</param>
+        /// <returns>User input of type int</returns>
+        public static int ReadInt(string message, int start, int end)
+        {
+            Console.WriteLine(message);
+            var data = Console.ReadLine().Trim();
+            int number;
+
+            while (!int.TryParse(data, out number) || number < start || number > end)
+            {
+                Console.WriteLine($"\n\nYou need to enter an integer number between {start} and {end}. Try again:");
+                data = Console.ReadLine().Trim();
+            }
+
+            return number;
+        }
+
+        /// <summary>
+        /// Accept user input from console window, if it is equal to the command parameter return true, else return false.
+        /// </summary>
+        /// <param name="message">Message shown when prompted for user input</param>
+        /// <param name="command">Expected user input to return true</param>
+        /// <param name="ignoreCase">Whether to ignore case when comparing command and user input (true if ignore)</param>
+        /// <returns>True if user input is equal to the command parameter, false if not.</returns>
+        public static bool ReadBoolOneTime(string message, string command, bool ignoreCase = false)
+        {
+            Console.WriteLine(message);
+            var data = Console.ReadLine();
+
+            if (ignoreCase)
+                return string.Equals(data, command, StringComparison.OrdinalIgnoreCase);
+            else
+                return string.Equals(data, command);
+        }
+    }
+}
