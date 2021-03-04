53228b7cac7c34f342e90a8fe067a24e73c4d4a7
MariiaRev <maria.revenko2209@gmail.com>
2021-03-04 17:28:00 +0200

    Added exception filter, AuthController and models for user and error

---
+++ HomeWork11/DepsWebApp/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DepsWebApp.Models
+{
+    /// <summary>
+    /// User model.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// User login.
+        /// </summary>
+        [Required]
+        public string Login { get; set; }
+
+        /// <summary>
+        /// User password.
+        /// </summary>
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Parameterless constructor.
+        /// </summary>
+        public User () { }
+
+        /// <summary>
+        /// Initializes a new instanse of the class <see cref="User"/> with <paramref name="login"/> and <paramref name="password"/>.
+        /// </summary>
+        /// <param name="login">The <see cref="Login"/> value.</param>
+        /// <param name="password">The <see cref="Password"/> value.</param>
+        public User (string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+}
