e1c6adf8135b9627781ddfecafc9a7b828e96c52
MariiaRev <maria.revenko2209@gmail.com>
2021-03-14 20:09:59 +0200

    Added tests for the web service

--- HomeWork12/DepsWebApp.Tests/Program.cs
+++ HomeWork12/DepsWebApp.Tests/Program.cs
@@ -6,31 +6,47 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text.Json;
 using static DepsWebApp.Tests.Tests;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace DepsWebApp.Tests
 {
     class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("This is Homework 11 Task 2. Tests for Web Service DepsWebApp\nMade by Mariia Revenko");
+            Console.WriteLine("This is Homework 12 Task 2. Tests for Web Service DepsWebApp\nMade by Mariia Revenko");
 
             var optionsPath = "options.json";
             try
             {
                 var baseAddress = await GetBaseAddress(optionsPath);
-                var client = new HttpClient
+
+                // client for unauthorized user
+                var client1 = new HttpClient
                 {
                     BaseAddress = new Uri(baseAddress)
                 };
 
+                // client for authorized user
+                (string login, string password) = ("login1", "1111111");
+                var encodedUserData = EncodeUserData(login, password);
+                var client2 = new HttpClient
+                {
+                    BaseAddress = new Uri(baseAddress)
+                };
+                client2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedUserData);
+                
                 var results = new List<(bool, string)>()
                 {
-                    await Test1(client),
-                    await Test2(client),
-                    await Test3(client),
-                    await Test4(client),
-                    await Test5(client)
+                    await Test1(client1),
+                    await Test2(client1),
+                    await Test3(client1, login, password),
+                    await Test4(client1),
+                    await Test5(client2),
+                    await Test6(client2),
+                    await Test7(client2),
+                    await Test8(client2)
                 };
 
                 results.ForEach(result => Console.WriteLine(result.Item2));
@@ -56,7 +72,7 @@ namespace DepsWebApp.Tests
             Console.WriteLine("\n\n\n");
         }
 
-        static async Task<string> GetBaseAddress(string path)
+        private static async Task<string> GetBaseAddress(string path)
         {
             var text = await File.ReadAllTextAsync(path);
             var json = JsonSerializer.Deserialize<Options>(text);
@@ -65,6 +81,14 @@ namespace DepsWebApp.Tests
                 throw new ArgumentNullException(nameof(json.BaseAddress), "Base address cannot be null");
 
             return json.BaseAddress;
+        }
+
+        private static string EncodeUserData(string login, string password)
+        {
+            var str = $"{login}: {password}";
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            return Convert.ToBase64String(bytes);
         }
     }
 }
